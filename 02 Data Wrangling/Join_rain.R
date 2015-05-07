ea8e7ddae18c6412daa18dd36df9f49c07859ca0
unknown <jaclyncnguyen@utexas.edu>
2015-05-07 18:24:38 -0500

    creation of facet plot

--- 02 Data Wrangling/Join_rain.R
+++ 02 Data Wrangling/Join_rain.R
@@ -19,5 +19,5 @@ tbl_df(canberrar)
   #join_table 
 #}
 join1 <- inner_join(adelaider, brisbaner, by = "YEAR")  %>% inner_join(cairnsr, by = "YEAR") %>% inner_join(canberrar, by = "YEAR") %>% inner_join(darwinr, by = "YEAR") %>% inner_join(hobartr, by = "YEAR") %>% inner_join(melbourner, by = "YEAR") %>% inner_join(perthr, by = "YEAR") %>% inner_join(sydneyr, by = "YEAR") %>% inner_join(townsviller, by = "YEAR")
-names(join1) <- c("YEAR", "Adelaide_Total", "Adelaride_AVG", "Brisbane_Total", "Brisbane_AVG", "Cairns_Total", "Cairns_AVG", "Canberra_Total", "Canberra_AVG", "Darwin_Total", "Darwin_AVG", "Hobart_Total", "Hobart_AVG", "Melbourne_Total", "Melbourne_AVG", "Perth_Total", "Perth_AVG", "Sydney_Total", "Sydney_AVG", "Townsville_Total", "Townsville_AVG" )
-join1 %>% ggplot()
+names(join1) <- c("YEAR", "Adelaide_Total", "Adelaide_AVG", "Brisbane_Total", "Brisbane_AVG", "Cairns_Total", "Cairns_AVG", "Canberra_Total", "Canberra_AVG", "Darwin_Total", "Darwin_AVG", "Hobart_Total", "Hobart_AVG", "Melbourne_Total", "Melbourne_AVG", "Perth_Total", "Perth_AVG", "Sydney_Total", "Sydney_AVG", "Townsville_Total", "Townsville_AVG" )
+join1 %>%  melt(id.vars = "YEAR", measure.vars = c("Adelaide_AVG", "Brisbane_AVG", "Cairns_AVG", "Canberra_AVG", "Darwin_AVG", "Hobart_AVG",  "Melbourne_AVG",  "Perth_AVG",  "Sydney_AVG", "Townsville_AVG")) %>% ggplot(aes(x = YEAR, y = value)) + geom_line(aes(color = variable)) + facet_wrap(~variable)
